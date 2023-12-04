9a363130e74e0095aa5f92ee28ad0dd0aff754a4
ScaraMocchie <talitarahmi@11@gmail.com>
2023-12-04 11:15:06 +0700

    add email validator and limit lenght of input

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -102,8 +102,10 @@ class _LoginPageState extends State<LoginPage> {
                         height: 70,
                         padding: EdgeInsets.all(10),
                         child: TextFormField(
+                          maxLength: 24,
                           controller: usernameController,
                           decoration: InputDecoration(
+                            counterText: '',
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20)),
                             hintText: "username",
@@ -119,8 +121,10 @@ class _LoginPageState extends State<LoginPage> {
                         padding: EdgeInsets.all(10),
                         child: TextFormField(
                           controller: passwordController,
+                          maxLength: 24,
                           obscureText: ok,
                           decoration: InputDecoration(
+                            counterText: '',
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20)),
                             hintText: "Password",
@@ -203,7 +207,7 @@ class _LoginPageState extends State<LoginPage> {
                             data=jsonDecode(response.body);
                             AccountData.isPrem=data[0];
                             AccountData.permissionStatus=data[3];
-                             AccountData.deadlinePermission=data[2];
+                            AccountData.deadlinePermission=data[2];
                             await  AccountData.getData();
                               Get.off(HomePage());}
                             else{setState(() {
