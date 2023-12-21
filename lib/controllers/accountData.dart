81f8a50fef355c6ac95f74a714502ba8e184189f
ScaraMocchie <talitarahmi@11@gmail.com>
2023-12-21 16:24:07 +0700

    add select avatar features

--- lib/controllers/accountData.dart
+++ lib/controllers/accountData.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 class AccountData{
+  static List avatarList = ['assets/images/avatars/Avatar01.svg', 'assets/images/avatars/Avatar03.svg', 'assets/images/avatars/Avatar04.svg', 'assets/images/avatars/Avatar05.svg', 'assets/images/avatars/Avatar06.svg', 'assets/images/avatars/Avatar07.svg', 'assets/images/avatars/Avatar08.svg', 'assets/images/avatars/Avatar09.svg', 'assets/images/avatars/Avatar10.svg', 'assets/images/avatars/Avatar11.svg', 'assets/images/avatars/Avatar12.svg', 'assets/images/avatars/Avatar13.svg', 'assets/images/avatars/Avatar14.svg', 'assets/images/avatars/Avatar15.svg', 'assets/images/avatars/Avatar16.svg', 'assets/images/avatars/Avatar17.svg', 'assets/images/avatars/Avatar18.svg', 'assets/images/avatars/Avatar19.svg', 'assets/images/avatars/Avatar20.svg', 'assets/images/avatars/Avatar21.svg'];
+  static int avatarIndex = 0;
   //basic accound data
   static int? isPrem;
   static String? username;
---
+++ lib/screens/image_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tobagen2/controllers/accountData.dart';
+import 'package:tobagen2/controllers/routes.dart';
+import 'package:tobagen2/screens/accountPage.dart';
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({super.key});
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  List<bool> selectedImages = List.generate(AccountData.avatarList.length, (index) => false);
+  int selectedImageIndex = AccountData.avatarIndex;
+  @override
+  Widget build(BuildContext context) {
+    Routes.tempContext=context;
+    return WillPopScope(child: Scaffold(
+      appBar: AppBar(leading: IconButton(onPressed: (){Routes.getBack();}, icon: Icon(Icons.arrow_back_ios_new_rounded)),centerTitle: true, title: Text("Select an image"),),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: AccountData.avatarList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedImageIndex= index;
+              });
+
+              print('Image at index $index selected');
+            },
+            child: Card(
+              color: selectedImageIndex == index ?  Color.fromARGB(255, 187, 254, 255): null,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AccountData.avatarList[index],
+                    fit: BoxFit.cover,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Text('Avatar '+ (index+1).toString()),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xff528DE7),
+        onPressed: () {
+          // Perform the action when the confirm button is clicked
+          print('Selected avatar index: $selectedImageIndex');
+          AccountData.avatarIndex = selectedImageIndex;
+          // Routes.tempContext=context;
+          Routes.getBack();
+        },
+        child: Icon(Icons.check, color: Colors.white,),
+      ),
+    ), onWillPop: ()async{
+      Routes.getBack();
+      return true;});
+  }
+}
