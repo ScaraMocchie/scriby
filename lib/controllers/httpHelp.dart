9fb0b35186f525cf440e0577d799c2b9e194244e
risal098 <yesitsme811@gmail.com>
2024-01-04 23:17:41 +0700

    add icon

--- lib/controllers/httpHelp.dart
+++ lib/controllers/httpHelp.dart
@@ -1,6 +1,6 @@
 class Helper{
   static String  baseUrl="www.bicaraai.com";
   static String baseApi="api/toba/";
-  static String baseWeb="www.bicaraai.com";
+  static String baseWeb="bicaraai.com";
 }
 
